c387d7ed55db6a299e871f8cbe636e97fd48507f
Catherine Li <catheryn@mit.edu>
2018-01-13 12:52:57 -0500

    Change lint

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,14 @@
 import * as React from 'react';
 import './App.css';
 
-const logo = require('./logo.svg');
+const logo = require('../logo.svg');
 
 class App extends React.Component {
+
+  handleStartGame() {
+    console.log('hi');
+  }
+
   render() {
     return (
       <div className="App">
@@ -12,7 +17,8 @@ class App extends React.Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
+          Start a new card game!
+          <button onClick={this.handleStartGame}>Start game</button>
         </p>
       </div>
     );
