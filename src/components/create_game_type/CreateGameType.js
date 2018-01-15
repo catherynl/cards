3d3fcb513defe12585311120d14fc49471cb7df9
Victoria Xia <vxia@mit.edu>
2018-01-15 10:48:13 -0500

    bare bones shell for game engine logic.

---
+++ src/components/create_game_type/CreateGameType.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import fire from '../fire';
+
+class CreateGameType extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+}
+
+export default CreateGameType;
