c6ae3f6e36cfcb7af862ebbd5e52f09d8eb3c663
Victoria Xia <vxia@mit.edu>
2018-01-14 23:31:36 -0500

    add capital letters as key bindings.

--- src/components/Hand.js
+++ src/components/Hand.js
@@ -5,7 +5,9 @@ import { range } from 'lodash';
 import Card from './Card';
 
 const KEYS = Array.of('q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd',
-                      'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm');
+                      'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm',
+                      'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D',
+                      'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M');
 
 class Hand extends Component {
 
